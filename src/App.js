be699d1162ca6b3ceb8b3148833ab1249ceb2256
sanamalk <sanamalik9568@gmail.com>
2025-01-29 23:40:14 +0530

    firstversion

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './index.css';
+import Stopwatch from './Component/stopwatch';
+
+function App() {
+  return (
+    <>
+     <Stopwatch/>
+
+    </>
+  );
+}
+
+export default App;
